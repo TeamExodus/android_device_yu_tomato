2270a88038127c7462c5c9389b3997f32351bbb3
Meninblack007 <sanyam.53jain@gmail.com>
2016-01-16 03:08:11 -0500

    Revert "tomato: Implement the healthd offline charger"
    This reverts commit 6ab60a7745de4d9d0ed5ff2710325c75679fa8a9.

--- device.mk
+++ device.mk
@@ -90,10 +90,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
-# Healthd to boot directly from charger mode rather than initiating a reboot.
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.enable_boot_charger_mode=1
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/goodix.kl:system/usr/keylayout/goodix.kl \
